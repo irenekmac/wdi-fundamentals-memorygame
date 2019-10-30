6b72bd25f8fef1ce251429c5600c93d715fe7c86
irenekmac <irenekaymac@gmail.com>
2019-10-31 03:26:28 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,21 @@
-console.log("Up and Running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardTwo);
+//console.log("Up and Running!");
+//console.log("User flipped" + cardOne);
+//console.log("User flipped" + cardTwo);
+
+alert('Hello, friends.');
+
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+if (cardsInPlay.length === 2) {
+} else if (cardsInPlay[0] === cardsInPlay[2]) {
+alert("You found a match!");
+} else (cardsInPlay[0] === cardsInPlay[2])
+alert("Sorry, try again");
